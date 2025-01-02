1cc05bb6d8600ef460e3239e4acc931723259068
Deepak Kumar <126335641+Deepak-Kumar45@users.noreply.github.com>
2025-01-02 07:25:54 +0000

    frontend, backend folders created

---
+++ backend/blue-mercury/src/main/java/com/bluemercury/blue_mercury/BlueMercuryApplication.java
@@ -0,0 +1,13 @@
+package com.bluemercury.blue_mercury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueMercuryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueMercuryApplication.class, args);
+	}
+
+}
---
+++ backend/blue-mercury/src/test/java/com/bluemercury/blue_mercury/BlueMercuryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluemercury.blue_mercury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueMercuryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
