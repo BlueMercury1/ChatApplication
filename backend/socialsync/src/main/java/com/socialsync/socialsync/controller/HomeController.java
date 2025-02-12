686b8c7387c1007d64656e31e02c37ee1e96347e
Deepak kumar <kumardeep4591@gmail.com>
2025-02-12 17:38:53 +0530

    Minor changes

--- backend/socialsync/src/main/java/com/socialsync/socialsync/controller/HomeController.java
+++ backend/socialsync/src/main/java/com/socialsync/socialsync/controller/HomeController.java
@@ -1,12 +1,35 @@
 package com.socialsync.socialsync.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
+    @GetMapping("/")
     public String home() {
-        return "home";
+        return "This is your home page";
+    }
+
+    @GetMapping("/auth/show")
+    public ResponseEntity<String> show() {
+        return ResponseEntity.ok("showing the auth page");
+    }
+
+    @GetMapping("/api/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("This is after login test page");
+    }
+
+    @GetMapping("/api/test1")
+    public ResponseEntity<String> test1() {
+        return ResponseEntity.ok("This is after login test page-1");
+    }
+
+    @GetMapping("/api/test2")
+    public ResponseEntity<String> test2() {
+        return ResponseEntity.ok("This is after login test page-2");
     }
 
 }
--- backend/socialsync/src/main/java/com/socialsync/socialsync/dto/SampleDto.java
+++ backend/socialsync/src/main/java/com/socialsync/socialsync/dto/SampleDto.java
@@ -1,5 +1,8 @@
 package com.socialsync.socialsync.dto;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SampleDto {
 
 }
